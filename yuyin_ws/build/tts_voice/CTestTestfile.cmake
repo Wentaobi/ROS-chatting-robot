18a04a5c90d49f07614bbe54b9400032be9c292a
Wentao Bi <biwentaofrank@gmail.com>
2018-02-03 20:17:21 -0500

    Add files via upload

---
+++ yuyin_ws/build/tts_voice/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wen/Desktop/yuyin_ws/src/tts_voice
+# Build directory: /home/wen/Desktop/yuyin_ws/build/tts_voice
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
